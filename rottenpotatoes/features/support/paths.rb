2e0753f9ae3b8093650c061d5b06fbd5e7ae0575
ShaoEnChen <seanchen47@gmail.com>
2018-10-05 17:35:37 +0000

    add 3 cucumber scenarios for create, update and delete

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -15,6 +15,9 @@ module NavigationHelpers
 
     when /^the( RottenPotatoes)? home\s?page$/
       movies_path
+      
+    when /^the create new movie page$/
+      new_movie_path
 
     when /^the edit page for "(.+)"$/
       edit_movie_path Movie.find_by_title($1)
